903f869b7e895a9c8e1e12a88dbd2a17126288ed
ZKYAbbot <1964483239@qq.com>
2017-10-15 01:37:11 +0800

    Kotlin Operators

--- src/Operate.kt
+++ src/Operate.kt
@@ -6,10 +6,32 @@
  * Description:
  *
  */
-fun main(args: Array<String>) {
+fun main(args: Array<String>)
+{
+    /**
+     * Example: Arithmetic Operators
+     */
     val number1 = 12.5
     val number2 = 3.5
-    var result:Double
-    result = number1+number2
-    print("number1+number2 ="+result)
+    var result: Double
+    result = number1 + number2
+    println("number1 + number2 = " + result)
+    result = number1 - number2
+    println("number1 - number2 = " + result)
+    result = number1 * number2
+    println("number1 * number2 = " + result)
+    result = number1/number2
+    println("number1 / number2 = $result")
+    result = number1 % number2
+    println("number1 % number2 = $result")
+
+    /**
+     * Example: Arithmetic Operators
+     */
+    println("Example: Arithmetic Operators")
+    val start= "I am a good coder."
+    val middle = "I like code the code."
+    val end = "-   Abbot"
+    val res = start+middle+end
+    println(res)
 }

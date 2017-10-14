15bdb4a49f471357d30e89ba6261d6a501bac2d1
ZKYAbbot <1964483239@qq.com>
2017-10-15 00:17:42 +0800

    Kitlon Study.

---
+++ src/Operate.kt
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Abbot
+ * Date: 2017-10-14
+ * Time: 23:56
+ * Description:
+ *
+ */
+fun main(args: Array<String>) {
+    val number1 = 12.5
+    val number2 = 3.5
+    var result:Double
+    result = number1+number2
+    print("number1+number2 ="+result)
+}
---
+++ src/app.kt
@@ -0,0 +1,33 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Abbot
+ * Date: 2017-10-13
+ * Time: 16:43
+ * Description:
+ *
+ */
+fun main(args : Array<String>)
+{
+    println("Hello World!")
+    var language="French"
+    val score=99
+    println(language)
+    print(score)
+    println()
+    var language1:String
+    language1="zky";
+    val score1:Int
+    score1 = 232
+    // var can change
+    // val not change like final value in java.
+
+    var range:Byte =112
+    println("range:"+"$range")
+
+
+    var letter:Char
+    letter='z'
+    println("letter:"+"$letter")
+    var flag = true
+    println("$flag")
+}

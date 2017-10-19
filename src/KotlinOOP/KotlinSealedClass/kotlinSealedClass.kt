88bc6ce521c02168ee95802786cb4500feaefcef
root <1964483239@qq.com>
2017-10-20 03:07:01 +0800

    Kotlin language study on ubuntu.

---
+++ src/KotlinOOP/KotlinSealedClass/kotlinSealedClass.kt
@@ -0,0 +1,40 @@
+package KotlinOOP.KotlinSealedClass
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: root
+ * Date: 10/20/17
+ * Time: 2:43 AM
+ * Description:
+ * Kotlin Sealed Classes
+ */
+fun main(args: Array<String>)
+{
+
+}
+
+//class Expr
+//class Const(val value:Int) :Expr
+//class Sum(val left:Expr,val right:Expr):Expr
+//fun eval(e:Expr):Int =
+//        when(e)
+//        {
+//            is Const ->e.value
+//            is Sum ->eval(e.right) + eval(e.left)
+//            else ->
+//                    throw IllegalArgumentException("Unknown expression")
+//        }
+
+
+sealed class Expr
+class Const(val value:Int):Expr()
+class Sum(val left:Expr,val right:Expr):Expr()
+object NotAnumber:Expr()
+
+fun eval(e:Expr) :Int =
+        when(e)
+        {
+            is Const ->e.value
+            is Sum -> eval(e.right) + eval(e.left)
+            NotAnumber -> java.lang.Double.NaN
+        }

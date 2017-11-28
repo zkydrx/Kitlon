8ecd74446ef775b5216fcf658d247f5a5b36743a
ZKYAbbot <1964483239@qq.com>
2017-11-28 17:07:13 +0800

    reStudy Kitlon.

---
+++ src/NovemberReStudyKitlon/gogogo.kt
@@ -0,0 +1,42 @@
+package NovemberReStudyKitlon
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Abbot
+ * Date: 2017-11-28
+ * Time: 16:38
+ * Description:
+ *
+ *
+ * 1-12 月份英文简写
+一月 January            简写 Jan.
+二月 February           简写 Feb.
+三月 March             简写 Mar.
+四月 April              简写 Apr.
+五月 May               简写 May
+六月 June               简写 Jun.
+七月 July                 简写 Jul.
+八月 August              简写 Aug.
+九月 September           简写 Sep.
+十月 October             简写 Oct.
+十一月 November          简写 Nov.
+十二月 December         简写 Dec.
+ */
+
+
+fun main(args: Array<String>)
+{
+    println("January")
+    println("February")
+    println("March")
+    println("April")
+    println("May")
+    println("June")
+    println("July")
+    println("Auguest")
+    println("September")
+    println("October")
+    println("November")
+    println("December")
+}
+
